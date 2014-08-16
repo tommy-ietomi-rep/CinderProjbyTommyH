d0948b1f7ffda726b357735f9eca00eaedccf160
ieietommy@gmail.com <ieietommy@gmail.com>
2014-08-16 15:16:54 +0900

    test2

---
+++ MyCinder/Resources.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "cinder/CinderResources.h"
+
+#define RES_PARTICLE		CINDER_RESOURCE( resource/, particle.png, 128, PNG )
+#define RES_EMITTER			CINDER_RESOURCE( resource/, emitter.png, 129, PNG )
+#define RES_NORMAL			CINDER_RESOURCE( resource/, normal.png, 130, PNG )
+#define RES_BUMP			CINDER_RESOURCE( resource/, bump.png, 131, PNG )
+#define RES_SPEC_EXPONENT	CINDER_RESOURCE( resource/, specExponent.png, 132, PNG )
+#define RES_EMITTER_VERT	CINDER_RESOURCE( resource/, emitter_vert.glsl, 128, GLSL )
+#define RES_EMITTER_FRAG	CINDER_RESOURCE( resource/, emitter_frag.glsl, 129, GLSL )
